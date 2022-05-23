75b58f8fd1d7285b9f51773b1a5565bf5a8ef66e
keyem4251 <kisk.emura@gmail.com>
2022-05-23 21:39:23 +0900

    PartialEqをderive

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
 }
 
 fn fn_2_1() {
+    #[derive(PartialEq)]
     struct FullName {
         first_name: String,
         last_name: String,
@@ -17,6 +18,11 @@ fn fn_2_1() {
         }
     }
 
+    // 値オブジェクトを作成
     let full_name = FullName::new("first".to_string(), "last".to_string());
     println!("{} {}", full_name.first_name, full_name.last_name);
+
+    // PartialEqで値が等価な場合にtrueを返す
+    let full_name2 = FullName::new("first".to_string(), "last".to_string());
+    println!("{}", full_name == full_name2);
 }
