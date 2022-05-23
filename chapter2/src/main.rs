3cbeaaf77b665d5229142450e674090cee696540
keyem4251 <kisk.emura@gmail.com>
2022-05-23 21:30:04 +0900

    FullName structを作成

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    fn_2_1();
+}
+
+fn fn_2_1() {
+    struct FullName {
+        first_name: String,
+        last_name: String,
+    }
+
+    impl FullName {
+        fn new(first_name: String, last_name: String) -> Self {
+            FullName {
+                first_name,
+                last_name,
+            }
+        }
+    }
+
+    let full_name = FullName::new("first".to_string(), "last".to_string());
+    println!("{} {}", full_name.first_name, full_name.last_name);
 }
