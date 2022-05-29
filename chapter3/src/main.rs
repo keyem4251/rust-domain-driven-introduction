838f1bc0d1da28d2e7a4518f82079716164b5062
keyem4251 <kisk.emura@gmail.com>
2022-05-29 16:44:20 +0900

    user entity

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    fn_3_1();
+}
+
+fn fn_3_1() {
+    #[derive(Debug)]
+    struct User {
+        name: String,
+    }
+
+    impl User {
+        fn new(name: String) -> Self {
+            User { name }
+        }
+
+        fn change_name(&mut self, name: String) {
+            if name.len() < 3 {
+                panic!("ユーザー名は3文字以上です");
+            }
+            self.name = name;
+        }
+    }
+
+    let mut user = User::new("test".to_string());
+    user.change_name("test2".to_string());
+    println!("{:?}", user);
 }
