b0a03df4469e0f7b07a1ad3e3ba7e354057f5bcb
keyem4251 <kisk.emura@gmail.com>
2022-05-31 21:44:41 +0900

    user repository

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn fn_5_1() {
+    struct UserName {
+        value: String,
+    }
+
+    impl UserName {
+        fn new(value: String) -> Self {
+            UserName { value }
+        }
+    }
+
+    struct User {
+        name: UserName,
+    }
+
+    impl User {
+        fn new(name: UserName) -> Self {
+            User { name }
+        }
+    }
+
+    struct UserService {
+        user_repository: Box<dyn UserRepository>,
+    }
+
+    impl UserService {
+        fn new(user_repository: Box<dyn UserRepository>) -> Self {
+            UserService { user_repository }
+        }
+
+        fn exists(&self, user: User) -> bool {
+            let found = self.user_repository.find(user.name);
+            match found {
+                None => false,
+                Some(_user) => true,
+            }
+        }
+    }
+
+    trait UserRepository {
+        fn save(&self, user: User);
+        fn find(&self, name: UserName) -> Option<User>;
+    }
+}
